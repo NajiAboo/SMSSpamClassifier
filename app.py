360ced65746cb56d0fe61ba96423332056a2aef0
USER <naji.aboo@triassicsolutions.com>
2022-08-01 07:48:50 +0530

    SMS message classification

---
+++ app.py
@@ -0,0 +1,40 @@
+from crypt import methods
+from os import stat
+from flask import Flask, current_app, render_template, request
+import pickle
+from entity.sms_spam_classifier import SMSSPAMClassifier
+import sys  
+import nltk
+sys.path.insert(0, '../SMSSpamClassifier/entity/')
+
+
+nltk.download('stopwords')
+    
+def load_classifier() -> SMSSPAMClassifier:
+    sms_classifier = None
+    with open('model/smsspamclassifer.pkl','rb') as picker_reader:
+        sms_classifier = pickle.load(picker_reader)
+    return sms_classifier
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    message_status = None
+    if request.method == "POST":
+        message_status = "Ham"
+        status = current_app.model.predict(request.form.get('review_msg'))
+        
+        if status[0] == 1:
+            message_status = "Spam"
+
+        #eturn status
+
+    return render_template('index.html',message_status=message_status)
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        current_app.model: SMSSPAMClassifier = load_classifier()
+    app.run()
---
+++ entity/sms_spam_classifier.py
@@ -0,0 +1,30 @@
+from typing import List
+import re
+from nltk.stem import PorterStemmer
+
+
+
+class SMSSPAMClassifier:
+    def __init__(self, cv, rfc, sp_word) -> None:
+        self.cv = cv
+        self.rfc = rfc
+        self.sp_words = sp_word
+        self.porter_stemmer = PorterStemmer()
+    
+    def clean_data(self, input_text) -> List:
+        corpus = []
+        review = re.sub('^[A-Za-z0-9]', ' ', input_text)
+        review = review.lower()
+        review = review.split()        
+        cp = [ self.porter_stemmer.stem(word, to_lowercase=True) for word in review if word not in self.sp_words]
+        cp = ' '.join(cp)
+        corpus.append(cp)
+        return corpus
+        
+
+    def predict(self, input_text):
+        x_clean = self.clean_data(input_text)
+        print(x_clean)
+        x_input = self.cv.transform(x_clean)
+        y_pred = self.rfc.predict(x_input)
+        return y_pred
